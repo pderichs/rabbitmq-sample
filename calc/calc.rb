8c24160986ed5f26742f9ddb05ea27c8f80b679c
pderichs <patderichs@gmail.com>
2015-02-19 21:41:13 +0100

    "Enhanced" "UI" :shipit:

--- calc/calc.rb
+++ calc/calc.rb
@@ -13,16 +13,19 @@ puts 'Waiting for work...'
 
 Thread.new do
   in_q.subscribe(manual_ack: true, block: true) do |delivery_info, properties, body|
-    puts 'Got new calc message'
-
     coord_ch = conn.create_channel
     coordinator_result_queue = coord_ch.queue('app.coordinator.results')
 
     task = Date.parse(body)
+    puts "Got new calc message -> #{task}"
 
+    # Simulate hard work
     sleep 3
 
+    # Send a "result" which is pretty positive... like... every time...
     coordinator_result_queue.publish("OK", persistent: true)
+
+    coord_ch.close
 
     ch.ack(delivery_info.delivery_tag)
   end
