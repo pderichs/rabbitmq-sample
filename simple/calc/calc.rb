624654d9907b08b97f336abb3279898aab2bda98
pderichs <patderichs@gmail.com>
2015-02-24 15:25:15 +0100

    Removing threads

--- simple/calc/calc.rb
+++ simple/calc/calc.rb
@@ -11,32 +11,33 @@ in_q = ch.queue('app.calc.tasks')
 
 puts 'Waiting for work...'
 
-Thread.new do
-  in_q.subscribe(manual_ack: true, block: true) do |delivery_info, properties, body|
-    coord_ch = conn.create_channel
-    begin
-      coordinator_result_queue = coord_ch.queue('app.calc.results')
 
-      task = JSON.parse(body)
-      puts "Got new calc message -> #{task}"
+in_q.subscribe(manual_ack: true) do |delivery_info, properties, body|
+  coord_ch = conn.create_channel
+  begin
+    coordinator_result_queue = coord_ch.queue('app.calc.results')
 
-      # Simulate hard work
-      # sleep 1
+    task = JSON.parse(body)
+    puts "Got new calc message -> #{task}"
 
-      # Send a "result"
-      task['calc_result'] = 42
-      coordinator_result_queue.publish(task.to_json, persistent: true)
-    rescue Exception => e
-      puts "ARGH! #{e}"
-    ensure
-      coord_ch.close
-    end
+    # Simulate hard work
+    # sleep 1
 
-    ch.ack(delivery_info.delivery_tag)
+    # Send a "result"
+    task['calc_result'] = 42
+    coordinator_result_queue.publish(task.to_json, persistent: true)
+  rescue Exception => e
+    puts "ARGH! #{e}"
+  ensure
+    coord_ch.close
   end
+
+  ch.ack(delivery_info.delivery_tag)
 end
 
 s = Readline.readline('> ')
 
 ch.close
 conn.close
+
+puts 'Bye.'
--- simple/packager/packager.rb
+++ simple/packager/packager.rb
@@ -11,49 +11,49 @@ in_q = ch.queue('app.packager.tasks')
 
 puts 'Waiting for work...'
 
-Thread.new do
-  in_q.subscribe(manual_ack: true, block: true) do |delivery_info, properties, body|
-    puts 'Got message'
+in_q.subscribe(manual_ack: true) do |delivery_info, properties, body|
+  puts 'Got message'
 
-    task = JSON.parse(body)
+  task = JSON.parse(body)
 
-    coord_ch = conn.create_channel
+  coord_ch = conn.create_channel
+  begin
+    coord_days_queue = coord_ch.queue('app.packager.daycount')
+
+    calc_ch = conn.create_channel
     begin
-      coord_days_queue = coord_ch.queue('app.packager.daycount')
+      calc_in_queue = calc_ch.queue('app.calc.tasks')
 
-      calc_ch = conn.create_channel
-      begin
-        calc_in_queue = calc_ch.queue('app.calc.tasks')
+      task = JSON.parse(body)
 
-        task = JSON.parse(body)
+      from = Date.strptime(task['from'],"%Y%m%d")
+      to = Date.strptime(task['to'],"%Y%m%d")
 
-        from = Date.strptime(task['from'],"%Y%m%d")
-        to = Date.strptime(task['to'],"%Y%m%d")
+      # Send day count to api
+      daysdiff = (to - from).to_i
+      puts "Diff in days: #{daysdiff}"
+      result = { 'task_id' => task['task_id'], 'day_count' => daysdiff }
+      coord_days_queue.publish(result.to_json, persistent: true)
 
-        # Send day count to api
-        daysdiff = (to - from).to_i
-        puts "Diff in days: #{daysdiff}"
-        result = { 'task_id' => task['task_id'], 'day_count' => daysdiff }
-        coord_days_queue.publish(result.to_json, persistent: true)
-
-        # Create tasks for calc - one task for each day
-        calc_task = { 'task_id' => task['task_id'] }
-        (from..to).each do |date|
-          calc_task['date'] = date.to_s
-          calc_in_queue.publish(calc_task.to_json, persistent: true)
-        end
-      ensure
-        calc_ch.close
+      # Create tasks for calc - one task for each day
+      calc_task = { 'task_id' => task['task_id'] }
+      (from..to).each do |date|
+        calc_task['date'] = date.to_s
+        calc_in_queue.publish(calc_task.to_json, persistent: true)
       end
     ensure
-      coord_ch.close
+      calc_ch.close
     end
-
-    ch.ack(delivery_info.delivery_tag)
+  ensure
+    coord_ch.close
   end
+
+  ch.ack(delivery_info.delivery_tag)
 end
 
 s = Readline.readline('> ')
 
 ch.close
 conn.close
+
+puts 'Bye.'
