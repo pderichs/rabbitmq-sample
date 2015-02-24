57b91cf926a619968f966485dae0c98dfbbf4eee
pderichs <patderichs@gmail.com>
2015-02-24 14:24:09 +0100

    Refactoring of api

--- simple/api/api.rb
+++ simple/api/api.rb
@@ -1,60 +1,102 @@
 require 'bunny'
 require 'json'
 require 'readline'
+require 'securerandom'
 
-conn = Bunny.new
-conn.start
+class ApiServer
+  CALC_RESULTS = 'app.calc.results'.freeze
+  PACKAGER_TASKS = 'app.packager.tasks'.freeze
 
-# Used to send tasks
-ch = conn.create_channel
-q = ch.queue('app.packager.tasks')
+  attr_reader :connection
 
-# Results from calc
-results_ch = conn.create_channel
-results_queue = results_ch.queue('app.coordinator.results') 
-
-# Day result from packager
-days_ch = conn.create_channel
-days_queue = days_ch.queue('app.coordinator.days')
-
-
-threads = []
-
-# Handling calc results
-threads << Thread.new do
-  results_queue.subscribe(manual_ack: true, block: true) do |delivery_info, properties, body|
-    puts 'Got calc result!'
-    puts ''
-
-    results_ch.ack(delivery_info.delivery_tag)
+  def initialize
+    initialize_bunny
+    create_packager_tasks_queue
+    create_calc_results_queue
+    create_packager_daycount_queue
+    subscribe_to_queues
   end
-end
 
-# Handling Day results
-threads << Thread.new do
-  days_queue.subscribe(manual_ack: true, block: true) do |delivery_info, properties, body|
-    days = body.to_i
-    puts "Got days: #{days}"
-    puts ''
+  def start_command_line
+    while true do
+      cmd = Readline.readline('> ')
 
-    days_ch.ack(delivery_info.delivery_tag)
+      case cmd
+      when 'send'
+        send
+      when 'quit', 'exit'
+        break
+      else
+        puts 'enter "send" or "quit" or "exit"'
+      end
+    end
   end
-end
 
-# "Command line"
-while true do
-  cmd = Readline.readline('> ')
+  def close
+    @calc_results_channel.close
+    @packager_daycount_channel.close
+    @channel.close
+    connection.close if @conn != nil
+  end
 
-  case cmd
-  when 'send' 
+  def send(task_count=1)
+    id = SecureRandom.uuid
     task = { 'from' =>'20120101', 'to' => '20120105'}
-    q.publish(task.to_json, persistent: true)
-  when 'quit' 
-    days_ch.close
-    results_ch.close
-    # It might be a bit hard, but... anyway... this is just a test right? ;-)
-    exit
-  else 
-    puts 'enter "send" or "quit"'  
+    @packager_tasks_queue.publish(task.to_json, persistent: true)
+  end
+
+  private
+
+  def initialize_bunny
+    @connection = Bunny.new
+    @connection.start
+  end
+
+  def create_packager_tasks_queue
+    @channel = connection.create_channel
+    @packager_tasks_queue = @channel.queue(PACKAGER_TASKS)
+  end
+
+  def create_calc_results_queue
+    @calc_results_channel = connection.create_channel
+    @calc_results_queue = @calc_results_channel.queue(CALC_RESULTS)
+  end
+
+  def create_packager_daycount_queue
+    @packager_daycount_channel = connection.create_channel
+    @packager_daycount_queue = @packager_daycount_channel.queue(CALC_RESULTS)
+  end
+
+  def subscribe_to_queues
+    opts = { manual_ack: true }
+
+    # Handling calc results
+    @calc_results_queue.subscribe(opts) do |delivery_info, properties, body|
+      puts 'Got calc result!'
+      puts ''
+      @calc_results_channel.ack(delivery_info.delivery_tag)
+    end
+
+    # Handling Day results
+    @packager_daycount_queue.subscribe(opts) do |delivery_info, _props, body|
+      days = body.to_i
+      puts "Got days: #{days}"
+      puts ''
+
+      @packager_daycount_channel.ack(delivery_info.delivery_tag)
+    end
   end
 end
+
+
+server = ApiServer.new
+begin
+  server.start_command_line
+rescue Exception => e
+  puts "ARGH! #{e}"
+ensure
+  puts 'Bye.'
+  puts ''
+  server.close
+end
+
